6a2ef2c02ba926dc02a5007a1ee232fe15f17958
ToxToxx <107263231+ToxToxx@users.noreply.github.com>
2023-09-16 20:48:01 +0300

    made player movement from left to right

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static Player Instance { get; private set; }
+    [SerializeField] private float moveSpeed = 5;
+    [SerializeField] private float rotatingSpeed = 60;
+    [SerializeField] private float maxMovingSpeedCoef = 1.5f;
+    [SerializeField] private Transform playerVisual;
+
+    private float maxMovingSpeed;
+    private float minMovingSpeed = 5f;
+
+
+    private bool isRunning;
+    private bool isWalking;
+
+    private Rigidbody rb;
+    private void Awake()
     {
-        
+        Instance = this;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (!gameObject.IsDestroyed())
+        {
+            HandleMovement();
+        }
+        Vector2 inputVector = GameInput.Instance.GetMovementVectorNormalized();
+        Vector3 moveDir = new(inputVector.x, 0f, 0f);
+        isWalking = moveDir != Vector3.zero;
+
+        transform.position += moveSpeed * Time.deltaTime * moveDir;
     }
+
+    private void HandleMovement()
+    {
+        
+        /*if (moveDir != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(moveDir, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotatingSpeed * Time.deltaTime);
+        }*/
+    }
+
 }
