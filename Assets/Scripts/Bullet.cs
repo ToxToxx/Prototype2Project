011052301925c91135f2212f02aab542effa6c56
ToxToxx <107263231+ToxToxx@users.noreply.github.com>
2023-09-17 21:11:58 +0300

    making bullet logic

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,14 +5,25 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     [SerializeField] private float bulletSpeed;
+    [SerializeField] private float bulletDestroyTime;
     private Vector3 shootDir;
     public void Setup(Vector3 shootDir)
     {
         this.shootDir = shootDir;
+        transform.eulerAngles = new Vector3(0,0, GetAngleFromVectorFloat(shootDir));
+        Destroy(gameObject, bulletDestroyTime);
     }
 
     private void Update()
     {
-        transform.position += shootDir * Time.deltaTime * bulletSpeed;
+        transform.position += bulletSpeed * Time.deltaTime * shootDir;
+    }
+
+    private float GetAngleFromVectorFloat(Vector3 dir)
+    {
+        dir = dir.normalized;
+        float n = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        if (n < 0) n += 360;
+        return n;
     }
 }
