2c2dba5b355ece2a50cdf0d8a6aa429d9b4fff8a
ToxToxx <107263231+ToxToxx@users.noreply.github.com>
2023-09-18 17:38:33 +0300

    made hit enemy logic

--- Assets/Scripts/Bullets/HitEnemy.cs
+++ Assets/Scripts/Bullets/HitEnemy.cs
@@ -1,11 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class HitEnemy : MonoBehaviour
 {
+    [SerializeField] private int damage;
+
+
+
     private void OnTriggerEnter(Collider other)
     {
+        if (other.GetComponent<Enemy>())
+        {
+            other.GetComponent<Enemy>().TakeDamage(damage);
+            Destroy(gameObject);
+        }
         
     }
 }
