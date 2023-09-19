070bf0bf22f1e9bf92248cf75f7da434a607dcd6
ToxToxx <107263231+ToxToxx@users.noreply.github.com>
2023-09-19 20:51:51 +0300

    fixing bugs

--- Assets/Scripts/EnemySpawner/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner/EnemySpawner.cs
@@ -7,9 +7,12 @@ public class EnemySpawner : MonoBehaviour
     public static EnemySpawner Instance;
 
     [SerializeField] private GameObject enemyPr;
-    [SerializeField] private float enemyRangeX;
+    [SerializeField] private int enemyRangeX;
     [SerializeField] private int enemyMax;
     [SerializeField] private int enemyCurrent;
+    [SerializeField] private float maxTimer;
+    private float currentTimer;
+    private float randomPositionX;
 
     private void Awake()
     {
@@ -18,13 +21,19 @@ public class EnemySpawner : MonoBehaviour
     private void Start()
     {
         enemyCurrent = 0;
+        currentTimer = 0;
     }
     private void Update()
-    {   
-        if(enemyCurrent < enemyMax)
+    {
+        randomPositionX = Random.Range(-enemyRangeX, enemyRangeX);
+        currentTimer += Time.deltaTime;
+
+        if(enemyCurrent < enemyMax && currentTimer > maxTimer)
         {
-            SpawnEnemy(enemyPr);
-            enemyCurrent++;  
+            SpawnEnemy(enemyPr, randomPositionX);
+
+            enemyCurrent++;
+            currentTimer = 0;
         }
     }
 
@@ -33,8 +42,9 @@ public class EnemySpawner : MonoBehaviour
         enemyCurrent--;
     }
 
-    private void SpawnEnemy(GameObject enemy)
+    private void SpawnEnemy(GameObject enemy, float randomPositionX)
     {
-        Instantiate(enemy, transform.position, Quaternion.identity);
+
+        Instantiate(enemy, new Vector3(randomPositionX, transform.position.y, transform.position.z), Quaternion.identity);
     }
 }
