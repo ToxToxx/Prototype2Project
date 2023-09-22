a9bae7518a6883e884e84c073ba7406957d77088
ToxToxx <107263231+ToxToxx@users.noreply.github.com>
2023-09-22 16:28:53 +0300

    making main menu and loading logic

---
+++ Assets/Scripts/MainMenu/TextHighlightLogic.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TextHighlightLogic : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private TextMeshProUGUI originalText;
+    private Color originalColor;
+    [SerializeField] private Color highlightColor; 
+
+    private void Start()
+    {
+        originalText = GetComponent<TextMeshProUGUI>();
+        originalColor = originalText.color;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        originalText.color = highlightColor;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        originalText.color = originalColor;
+    }
+}
