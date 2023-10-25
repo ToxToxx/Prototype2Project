8368ec0027e23bae73cfb2faea8e7c052fe8c3d0
ToxToxx <107263231+ToxToxx@users.noreply.github.com>
2023-10-25 19:38:40 +0300

    refactored manager's code

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -1,37 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
     public static SoundManager Instance { get; private set; }
-    [SerializeField] private AudioClipRefsSO audioClipRefsSO;
-    private float _volume = 1.0f;
+
+    [SerializeField] private AudioClipRefsSO _audioClipRefsSO;
+
+    private readonly float _volume = 1.0f;
 
     private void Awake()
     {
         Instance = this;
     }
-    private void Start()
+    private void OnEnable()
     {
         GameInput.Instance.OnShoot += GameInput_OnShoot;
         GameInput.Instance.OnHeal += GameInput_OnHeal;
         DefensePoint.Instance.OnPlayerGetDamage += DefensePoint_OnPlayerGetDamage;
     }
+    private void OnDisable()
+    {
+        GameInput.Instance.OnShoot -= GameInput_OnShoot;
+        GameInput.Instance.OnHeal -= GameInput_OnHeal;
+        DefensePoint.Instance.OnPlayerGetDamage -= DefensePoint_OnPlayerGetDamage;
+    }
 
     private void DefensePoint_OnPlayerGetDamage(object sender, System.EventArgs e)
     {
-        PlaySound(audioClipRefsSO.hitSound, Player.Instance.transform.position);
+        PlaySound(_audioClipRefsSO.hitSound, Player.Instance.transform.position);
     }
 
     private void GameInput_OnHeal(object sender, System.EventArgs e)
     {
-        PlaySound(audioClipRefsSO.healSound, Player.Instance.transform.position);
+        PlaySound(_audioClipRefsSO.healSound, Player.Instance.transform.position);
     }
 
     private void GameInput_OnShoot(object sender, System.EventArgs e)
     {
-        PlaySound(audioClipRefsSO.shootSound, Player.Instance.transform.position);
+        PlaySound(_audioClipRefsSO.shootSound, Player.Instance.transform.position);
     }
 
     private void PlaySound(AudioClip audioClip, Vector3 position, float volumeMultiplier = 50f)
@@ -39,10 +45,5 @@ public class SoundManager : MonoBehaviour
         AudioSource.PlayClipAtPoint(audioClip, position, volumeMultiplier * _volume);
     }
 
-    private void OnDestroy()
-    {
-        GameInput.Instance.OnShoot -= GameInput_OnShoot;
-        GameInput.Instance.OnHeal -= GameInput_OnHeal;
-        DefensePoint.Instance.OnPlayerGetDamage -= DefensePoint_OnPlayerGetDamage;
-    }
+
 }
